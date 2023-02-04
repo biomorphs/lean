ff0c3c476588453a918879f48b3cfe64f90dfbba
biomorphs <matt.hoyle@gmail.com>
2023-02-04 04:56:29 -0500

    More graph testing

--- source/graphs/graph_system.cpp
+++ source/graphs/graph_system.cpp
@@ -293,8 +293,8 @@ namespace Graphs
 
 		void PreparePinStorage();
 		std::vector<int> m_allIntPinValues;
-		robin_hood::unordered_flat_map<uint32_t, uint32_t> m_pinToStorageLookup;
-		//std::unordered_map<uint32_t, uint32_t> m_pinToStorageLookup;
+		//robin_hood::unordered_flat_map<uint32_t, uint32_t> m_pinToStorageLookup;
+		std::unordered_map<uint32_t, uint32_t> m_pinToStorageLookup;
 	};
 
 	GraphContext::GraphContext(Graph* graphToRun)
@@ -531,10 +531,41 @@ public:
 	}
 };
 
+class IntGreaterThan : public Graphs::Node
+{
+public:
+	IntGreaterThan()
+	{
+		m_name = "GreaterThan<int>";
+		AddInputPin(0, "Execute", "Execution");
+		AddInputPin(1, "Value1", "int");
+		AddInputPin(2, "Value2", "int");
+		AddOutputPin(3, "RunIfTrue", "Execution");
+		AddOutputPin(4, "RunIfFalse", "Execution");
+	}
+
+	ExecResult Execute(Graphs::GraphContext& ctx) override
+	{
+		SDE_PROF_EVENT();
+		int v1 = ctx.ReadPin(1);
+		int v2 = ctx.ReadPin(2);
+		if (v1 > v2)
+		{
+			ctx.ExecuteNext(m_id, 3);
+		}
+		else
+		{
+			ctx.ExecuteNext(m_id, 4);
+		}
+		return ExecResult::Completed;
+	}
+};
+
 class PrintIntNode : public Graphs::Node
 {
 public:
-	PrintIntNode()
+	PrintIntNode(std::string_view prefix = "Value = ")
+		: m_prefix(prefix)
 	{
 		m_name = "PrintValue<int>";
 		AddInputPin(0, "Execute", "Execution");
@@ -545,10 +576,11 @@ public:
 	{
 		SDE_PROF_EVENT();
 		int value = ctx.ReadPin(1);
-		SDE_LOG("%d", value);
+		SDE_LOG("%s%d", m_prefix.c_str(), value);
 		ctx.ExecuteNext(m_id, 2);	// run the next execution pin 
 		return ExecResult::Completed;
 	}
+	std::string m_prefix;
 };
 
 Graphs::Graph g_testGraph;
@@ -558,23 +590,42 @@ bool GraphSystem::Initialise()
 	SDE_PROF_EVENT();
 
 	g_testGraph.AddInputPin(0, "Execute", "Execution");
-	g_testGraph.AddOutputPin(1, "RunNext", "Execution");
-	g_testGraph.AddOutputPin(2, "Result", "int");
+	g_testGraph.AddInputPin(1, "TestValue", "int");
+	g_testGraph.AddOutputPin(2, "RunNext", "Execution");
+	g_testGraph.AddOutputPin(3, "Result", "int");
 
 	uint16_t c0id = g_testGraph.AddNode(new ConstantNode(3));
+	uint16_t printC0Id = g_testGraph.AddNode(new PrintIntNode(""));
 	uint16_t c1id = g_testGraph.AddNode(new ConstantNode(5));
+	uint16_t printC1Id = g_testGraph.AddNode(new PrintIntNode(" + "));
 	uint16_t addIntId = g_testGraph.AddNode(new AddIntsNode);
-	uint16_t printIntId = g_testGraph.AddNode(new PrintIntNode());
+	uint16_t printResultId = g_testGraph.AddNode(new PrintIntNode(" = "));
+	uint16_t igtId = g_testGraph.AddNode(new IntGreaterThan());
+	uint16_t printGtId = g_testGraph.AddNode(new PrintIntNode("which is greater than "));
+	uint16_t printNotGtId = g_testGraph.AddNode(new PrintIntNode("which is not greater than "));
 
-	g_testGraph.AddConnection(Graphs::Node::InvalidID, 0, addIntId, 0);		// graph execute -> addInt execute
-	g_testGraph.AddConnection(c0id, 0, addIntId, 1);						// c0 value -> addInt v1
-	g_testGraph.AddConnection(c1id, 0, addIntId, 2);						// c1 value -> addInt v2
-	g_testGraph.AddConnection(addIntId, 4, printIntId, 1);					// addInt result -> print value
-	g_testGraph.AddConnection(addIntId, 4, Graphs::Node::InvalidID, 2);		// addInt result -> graph result
+	// exec flow
+	g_testGraph.AddConnection(-1, 0, printC0Id, 0);				// graph execute -> c0 print
+	g_testGraph.AddConnection(printC0Id, 2, printC1Id, 0);		// c0 print -> c1 print
+	g_testGraph.AddConnection(printC1Id, 2, addIntId, 0);		// c1 print -> addInt execute
+	g_testGraph.AddConnection(addIntId, 3, printResultId, 0);	// addInt exec -> print reult
+	g_testGraph.AddConnection(printResultId, 2, igtId, 0);		// print result -> isIntGreater
+	g_testGraph.AddConnection(igtId, 3, printGtId, 0);			// int greater true -> print greater
+	g_testGraph.AddConnection(igtId, 4, printNotGtId, 0);		// int greater false -> print not greater
+	g_testGraph.AddConnection(printGtId, 2, -1, 2);				// print greater -> graph out
+	g_testGraph.AddConnection(printNotGtId, 2, -1, 2);			// print not greater -> graph out
 
-	g_testGraph.AddConnection(addIntId, 3, printIntId, 0);					// addInt exec -> print exec
-
-	g_testGraph.AddConnection(printIntId, 2, Graphs::Node::InvalidID, 1);	// print exec -> graph out exec
+	// data flow
+	g_testGraph.AddConnection(c0id, 0, addIntId, 1);			// c0 value -> addInt v1
+	g_testGraph.AddConnection(c1id, 0, addIntId, 2);			// c1 value -> addInt v2
+	g_testGraph.AddConnection(c0id, 0, printC0Id, 1);			// c0 value -> printC0
+	g_testGraph.AddConnection(c1id, 0, printC1Id, 1);			// c1 value -> printC1
+	g_testGraph.AddConnection(addIntId, 4, printResultId, 1);	// addInt result -> print value
+	g_testGraph.AddConnection(addIntId, 4, -1, 3);				// addInt result -> graph result
+	g_testGraph.AddConnection(addIntId, 4, igtId, 1);			// addInt result -> igt v1
+	g_testGraph.AddConnection(-1, 1, igtId, 2);					// graph input test value -> igt v2
+	g_testGraph.AddConnection(-1, 1, printGtId, 1);				// graph input test value -> print greater
+	g_testGraph.AddConnection(-1, 1, printNotGtId, 1);			// graph input test value -> print not greater
 	
 	return true;
 }
@@ -584,7 +635,8 @@ bool GraphSystem::Tick(float timeDelta)
 	SDE_PROF_EVENT();
 	{
 		Graphs::GraphContext runContext(&g_testGraph);
-		runContext.ExecuteNext(Graphs::Node::InvalidID, 0);						// execute graph pin 0
+		runContext.WriteGraphPin(Graphs::Node::InvalidID, 1, 7);		// test value
+		runContext.ExecuteNext(Graphs::Node::InvalidID, 0);				// execute graph pin 0
 		bool graphRunning = true;
 		while (graphRunning)
 		{
@@ -592,7 +644,7 @@ bool GraphSystem::Tick(float timeDelta)
 			if (stepResult == Graphs::GraphContext::Completed)
 			{
 				SDE_LOG("Graph finished ok");
-				int resultValue = runContext.ReadGraphPin(Graphs::Node::InvalidID, 2);
+				int resultValue = runContext.ReadGraphPin(Graphs::Node::InvalidID, 3);
 				SDE_LOG("\tresult = %d", resultValue);
 				graphRunning = false;
 			}
